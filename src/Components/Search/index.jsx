1ed5424aab1b04f68e6ab4b81c6e80bcdcc32757
Lucas Rodrigues <lukinhasr1423@gmail.com>
2023-01-01 20:13:27 -0300

    criado a lógica de filtragem

--- src/Components/Search/index.jsx
+++ src/Components/Search/index.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import styles from './Search.module.css'
 import lupa from '../../assets/images/lupa.svg'
 
-export default function Search() {
+export default function Search({ value, onChange }) {
   return (
     <div className={styles.search}>
-      <input type="text" />
+      <input
+       type="text"
+       value={value}
+       onChange={onChange}
+      />
       <button><img src={lupa} alt="Buscar" /></button>
     </div>
   )
--- src/Pages/Inicio/index.jsx
+++ src/Pages/Inicio/index.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import styles from './Inicio.module.css'
 import ProdutoCard from 'Components/ProdutoCard'
 import produtos from '../../json/produtos.json'
 import Search from 'Components/Search'
 
 export default function Inicio() {
+  const [busca, setBusca] = useState('')
+  
+  const produtosFiltrados = useMemo(() => {
+    const lawerBusca = busca.toLowerCase()
+    return produtos.filter((produto) => produto.titulo.toLowerCase().includes(lawerBusca))
+  }, [busca])
+
   return (
-    <>
-      
-      <section>
-        <div className={styles.buscar}>
-          <Search />
-        </div>
-        <div className={styles.produtos}>
-          {produtos.map((produto) => {
-            return <ProdutoCard 
-              {...produto}
-              key={produto.id} 
-            />
-          })}
-        </div>
-      </section>
-    </>
+    <section>
+      <div className={styles.buscar}>
+        <Search
+        value={busca}
+          onChange={(evento) => {
+            setBusca(evento.target.value)
+          }}
+        />
+      </div>
+      <div className={styles.produtos}>
+        {produtosFiltrados.map((produto) => {
+          return <ProdutoCard
+            {...produto}
+            key={produto.id}
+          />
+        })}
+      </div>
+    </section>
   )
 }
