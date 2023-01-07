408935656ec7eceece0cb39bb087d100cc0f0656
Lucas Rodrigues <lukinhasr1423@gmail.com>
2023-01-06 21:46:52 -0300

    ajustado a pag de carrinho

--- src/Pages/Carrinho/index.jsx
+++ src/Pages/Carrinho/index.jsx
@@ -1,8 +1,16 @@
+import ProdutoCard from 'Components/ProdutoCard'
 import React from 'react'
 import styles from './Carrinho.module.css'
 
 export default function Carrinho() {
   return (
-    <div>Carrinho</div>
+    <section className={styles.container}>
+      <ProdutoCard
+        id="5"
+        imagem="https://github.com/rodrigues14.png"
+        preco="R$ 350,00"
+        titulo="Teste Teste"
+      />
+    </section>
   )
 }
